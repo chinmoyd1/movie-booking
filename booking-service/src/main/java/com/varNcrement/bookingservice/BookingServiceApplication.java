4d13eb3ae5875c123d76a96b115d583987a863b2
chinmoy1113 <chinmoy1113@gmail.com>
2019-12-15 02:15:39 +0530

    kafka-address

--- booking-service/src/main/java/com/varNcrement/bookingservice/BookingServiceApplication.java
+++ booking-service/src/main/java/com/varNcrement/bookingservice/BookingServiceApplication.java
@@ -5,13 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import java.util.*;
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
 public class BookingServiceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookingServiceApplication.class, args);
 	}
-
 }
+
